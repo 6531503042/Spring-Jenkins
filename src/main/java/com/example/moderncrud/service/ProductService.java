7dc191e29658ea760f0e82c96e478ca1e725e832
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-12-06 22:05:09 +0700

    Backend: Added Service.

---
+++ src/main/java/com/example/moderncrud/service/ProductService.java
@@ -0,0 +1,49 @@
+package com.example.moderncrud.service;
+
+import com.example.moderncrud.entity.Product;
+import com.example.moderncrud.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll().stream()
+                .filter(product -> product.getPrice() != null)
+                .collect(Collectors.toList());
+    }
+
+    public Optional<Product> getProductById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public Optional<Product> updateProduct(Long id, Product updatedProduct) {
+        return productRepository.findById(id)
+                .map(existingProduct -> {
+                    existingProduct.setName(updatedProduct.getName());
+                    existingProduct.setDescription(updatedProduct.getDescription());
+                    existingProduct.setPrice(updatedProduct.getPrice());
+                    return productRepository.save(existingProduct);
+                });
+    }
+
+    public boolean deleteProduct(Long id) {
+        return productRepository.findById(id)
+                .map(product -> {
+                    productRepository.delete(product);
+                    return true;
+                })
+                .orElse(false);
+    }
+} 
