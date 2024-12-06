4ffbe702efd54ab1e7378c45123852def13917c6
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-12-06 22:05:23 +0700

    Create ModernCrudApplication.java

---
+++ src/main/java/com/example/moderncrud/ModernCrudApplication.java
@@ -0,0 +1,11 @@
+package com.example.moderncrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernCrudApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ModernCrudApplication.class, args);
+    }
+} 
